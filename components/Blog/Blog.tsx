d04d41c97e05a0df8c451e29363aaf4f289444f6
magdielian-code <oziomaagaecheta81@gmail.com>
2024-09-05 01:15:50 +0100

    feat: started blog page.

--- components/Blog/Blog.tsx
+++ components/Blog/Blog.tsx
@@ -1,12 +1,39 @@
-import React from 'react'
-import NavContainer from '../Utils/NavContainer'
+import React from "react";
+import NavContainer from "../Utils/NavContainer";
+import {
+  Flex,
+  Card,
+  Grid,
+  GridItem,
+  CardHeader,
+  CardBody,
+  CardFooter,
+} from "@chakra-ui/react";
 
 const Blog = () => {
   return (
     <NavContainer>
-        <div>Blog</div>
+      <Grid templateColumns="1fr 2fr 1fr" gap={0.7}>
+        <GridItem w="100%" h="100vh" bg="white" boxShadow={"lg"}>
+          <Flex flexDirection={'column'}>
+            <Card align={'start'}>
+              <CardHeader>{blog.catchPhrase}</CardHeader>
+              <CardBody>{blog.subtitle}</CardBody>
+              <CardFooter>
+                <Flex></Flex>
+              </CardFooter>
+            </Card>
+          </Flex>
+        </GridItem>
+        <GridItem w="100%" h="100vh" bg="white" boxShadow={"lg"}>
+          <Flex></Flex>
+        </GridItem>
+        <GridItem w="100%" h="100vh" bg="white" boxShadow={"lg"}>
+          <Flex></Flex>
+        </GridItem>
+      </Grid>
     </NavContainer>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
